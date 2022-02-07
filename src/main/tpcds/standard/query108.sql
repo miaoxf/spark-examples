2bf6ebe72c8a6ab0c1ca4634992e7f9ab41eb023
jeffrey.miao <jeffrey.miao@vipshop.com>
2022-02-07 20:26:11 +0800

    fix[#22]: 108 109

---
+++ src/main/tpcds/standard/query108.sql
@@ -0,0 +1,17 @@
+--测试create as select
+drop table if exists vipdmt.query109_standard;
+create table vipdmt.query109_standard as
+select
+    ts,
+    pid,
+    containerid,
+    physical_mem_used,
+    physical_mem_cap,
+    virtual_mem_used,
+    virtual_mem_cap,
+    request_vcore,
+    cpu_useage_pct_per_core,
+    cpu_usage_pct_total,
+    char_read,
+    char_write
+from vipdmt.container_monitor_impl where dt='20220113' ;
